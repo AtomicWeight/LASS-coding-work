0ee1108bf652368f91297b51a0fb3b7a191b47c9
AtomicWeight <55098884+AtomicWeight@users.noreply.github.com>
2020-01-15 10:36:11 -0500

    Update Sub Cipher.py

--- Topic B work/Topic B assignments/Sub Cipher/Sub Cipher.py
+++ Topic B work/Topic B assignments/Sub Cipher/Sub Cipher.py
@@ -3,24 +3,31 @@ highLimit = ord('z')
 codeShift = 3
 codeText = ""
 vowels = 'aAeEiIoOuU'
+specialLetters = 'yYzZxX'
 subLetterE = ['!', '1']
 subLetterA = ['@', '2']
 subLetterI = ['#', '3']
 subLetterO = ['$', '4']
 subLetterU = ['%', '5']
+specialLetterY = ['^', '6']
+specialLetterZ = ['&', '7']
+specialLetterX = ['*', '8']
 plainText = input('Your Sentence:\n')
 loopCount1 = 0
 loopCount2 = 0
 loopCount3 = 0
 loopCount4 = 0
 loopCount5 = 0
+loopCount6 = 0
+loopCount7 = 0
+loopCount8 = 0
 
 
 
 for letter in plainText:
   if letter.isalpha() == True:
     code = ord(letter)
-    if letter not in vowels:
+    if letter not in vowels and letter not in specialLetters:
       if code >= lowLimit and code <= highLimit :
         code = code + codeShift
         if (code > highLimit):
@@ -52,6 +59,21 @@ for letter in plainText:
         if loopCount5 >= 1:
           letter = subLetterU[1]
         loopCount5 += 1
+      if letter in 'yY':
+        letter = specialLetterY[0]
+        if loopCount6 >= 1:
+          letter = specialLetterY[1]
+        loopCount6 += 1
+      if letter in 'xX':
+        letter = specialLetterX[0]
+        if loopCount7 >= 1:
+          letter = specialLetterX[1]
+        loopCount7 += 1
+      if letter in 'zZ':
+        letter = specialLetterZ[0]
+        if loopCount8 >= 1:
+          letter = specialLetterZ[1]
+        loopCount8 += 1
       newLetter = letter
     codeText = codeText + newLetter
   else:
@@ -59,40 +81,68 @@ for letter in plainText:
 print('')
 print('the encoded message is:\n', codeText)
 
-#Decoder
+code = open("encodedMessege.txt", 'w+')
+code.write(codeText)
+code.close()
+
+#Decoder 
+
+encodedFile = open("encodedMessege.txt", "r+")
+theMessege = open("theMessege.txt", "w+")
 
 oldText = ""
 oldSubLetter = ['!', '1', '@', '2', '#', '3', '$', '4', '%', '5']
+oldSpecialLetters = ['^', '6', '&', '7', '*', '8']
 oldSubLetterE = ['!', '1']
 oldSubLetterA = ['@', '2']
 oldSubLetterI = ['#', '3']
 oldSubLetterO = ['$', '4']
 oldSubLetterU = ['%', '5']
+oldSpecialLetterY = ['^', '6']
+oldSpecialLetterZ = ['&', '7']
+oldSpecialLetterX = ['*', '8']
 loopCount = 0
 
-for letter in codeText:
-  if letter.isalpha() == True:
-    code = ord(letter)
-    if code >= lowLimit and code <= highLimit :
-      code = code - codeShift
-      if (code > highLimit):
-        code = code + highLimit - lowLimit
+for line in encodedFile:
+  for letter in line:
+    if letter.isalpha() == True:
+      code = ord(letter)
+      if code >= lowLimit and code <= highLimit :
         code = code - codeShift
-    oldLetter = chr(code)
-  else:
-    if letter in oldSubLetterE:
-      letter = 'e'
-    if letter in oldSubLetterA:
-      letter = 'a'
-    if letter in oldSubLetterO:
-      letter = 'o'
-    if letter in oldSubLetterI:
-      letter = 'i'
-    if letter in oldSubLetterU:
-      letter = 'u'
-    oldLetter = letter
-  oldText = oldText + oldLetter
-  loopCount += 1
+        if code > highLimit:
+          code = code + highLimit - lowLimit
+          code = code - 6
+      oldLetter = chr(code)
+      loopCount += 1
+    elif letter in oldSubLetter or letter in oldSpecialLetters:
+      if letter in oldSubLetterE:
+        letter = 'e'
+      if letter in oldSubLetterA:
+        letter = 'a'
+      if letter in oldSubLetterO:
+        letter = 'o'
+      if letter in oldSubLetterI:
+        letter = 'i'
+      if letter in oldSubLetterU:
+        letter = 'u'
+      if letter in oldSpecialLetterX:
+        letter = 'x'
+      if letter in oldSpecialLetterZ:
+        letter = 'z'
+      if letter in oldSpecialLetterY:
+        letter = 'y'
+      oldLetter = letter
+      loopCount += 1
+    else:
+      oldLetter = letter
+    oldText = oldText + oldLetter
+
+theMessege.write(oldText)
 
 print('')
-print(' the message was:\n', oldText, '\n', 'the amount of characters in your text was\n', loopCount)
+print('the message was:\n', oldText)
+print('')
+print('the amount of characters in your text was:\n', loopCount)
+
+theMessege.close()
+encodedFile.close()
